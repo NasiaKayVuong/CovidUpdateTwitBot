86b8bdf592d69d292fa756cb5832fdfe13474a7e
NasiaKayVuong <nasiavuong@Nasias-MacBook-Pro.local>
2021-01-16 13:30:21 -0500

    Finished up the process of getting the daily average from the CDC. And the added data_NYT.py to get the county level data from the NYT.

---
+++ data-collection/daily-average.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+url = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=US_MAP_DATA"
+def get_daily_avg(link):
+    response = requests.get(link)
+    cdc_data = json.loads(response.text)
+    daily_avg = cdc_data["US_MAP_DATA"][-1]["Seven_day_avg_new_cases_per_100k"]
+    print(cdc_data["US_MAP_DATA"][-1]["Seven_day_avg_new_cases_per_100k"])
+    return daily_avg
+
+get_daily_avg(url)
